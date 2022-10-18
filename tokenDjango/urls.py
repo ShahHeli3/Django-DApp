d39820fea23305d8239991e2ea92efe3f17c4719
helis.inexture <helis.inexture@gmail.com>
2022-10-18 19:26:30 +0530

    MetaMask wallet connection

--- tokenDjango/urls.py
+++ tokenDjango/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('heliToken.urls')),
+    path('web3_auth/', include('web3_auth.urls')),
+    path('auth/', include('django.contrib.auth.urls')),
 ]
